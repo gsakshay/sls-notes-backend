d37ef98284750b5f6760c9f8e8d9c513408b6632
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-07-26 14:01:46 -0400

    update and getNotes

--- api/updateNote.js
+++ api/updateNote.js
@@ -14,12 +14,30 @@ import { getResponseHeaders } from "./utils"
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 const tableName = process.env.NOTES_TABLE
 
+import moment from "moment"
+
 exports.handler = async (event) => {
 	try {
+		let item = JSON.parse(event.body).Item
+		item.user_id = getUserID(event.headers)
+		item.user_name = getUserName(event.headers)
+		item.expires = moment().add(90, "days").unix()
+		// PUT class of DynamoDB
+		let data = await dynamoDB
+			.put({
+				TableName: tableName,
+				Item: item,
+				ConditionExpression: "#t = :t",
+				ExpressionAttributeNames: {
+					"#t": "timestamp",
+					":t": item.timestamp,
+				},
+			})
+			.promise()
 		return {
 			statusCode: 200,
 			headers: getResponseHeaders(),
-			body: JSON.stringify({}),
+			body: JSON.stringify(item),
 		}
 	} catch (e) {
 		console.log("Error", e)
