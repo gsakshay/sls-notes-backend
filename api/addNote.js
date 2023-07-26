6617b1ff06b9f0509489f6381c3895bd5b8b257b
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-07-26 13:45:20 -0400

    completed add function

--- api/addNote.js
+++ api/addNote.js
@@ -9,17 +9,35 @@ AWS.config.update({
 	region: "us-east-2",
 })
 
-import { getResponseHeaders } from "./utils"
+import { getResponseHeaders, getUserID, getUserName } from "./utils"
+
+import moment from "moment"
+import uuidv4 from "uuid/v4"
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 const tableName = process.env.NOTES_TABLE
 
 exports.handler = async (event) => {
 	try {
+		let item = JSON.parse(event.body).Item
+		item.user_id = getUserID(event.headers)
+		item.user_name = getUserName(event.headers)
+		item.note_id = item.user_id + ":" + uuidv4()
+		item.timestamp = moment().unix()
+		item.expires = moment().add(90, "days").unix()
+
+		// PUT class of DynamoDB
+		let data = await dynamoDB
+			.put({
+				TableName: tableName,
+				Item: item,
+			})
+			.promise()
+
 		return {
 			statusCode: 200,
 			headers: getResponseHeaders(),
-			body: JSON.stringify({}),
+			body: JSON.stringify(item),
 		}
 	} catch (e) {
 		console.log("Error", e)
